707beef49a49be5686754f519ec0dbd27ed56911
SirBastian80 <93028059+SirBastian80@users.noreply.github.com>
2022-09-09 18:04:43 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
+input.onButtonPressed(Button.A, function () {
+    PositionX += 1
+    if (PositionX <= 0) {
+        PositionX = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    PositionX += -1
+    if (PositionX >= 5) {
+        PositionX = 5
+    }
+})
+let PositionX = 0
+basic.showString("hi!")
+PositionX = 3
 basic.forever(function () {
-	
+    if (PositionX == 1) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    } else if (PositionX == 2) {
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            `)
+    } else if (PositionX == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (PositionX == 4) {
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            `)
+    } else if (PositionX == 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+    }
 })
